0c49b0b84143247bdce52d37d85cb5015d4ed082
molomojc <molomojacob8@gmail.com>
2025-06-07 22:59:30 +0300

    update

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// filepath: c:\Users\Jacob\Documents\FinalPortfolio\FinalProj\tailwind.config.js
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
